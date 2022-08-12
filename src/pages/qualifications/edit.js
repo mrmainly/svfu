493734202333bfb66d3b6ffebcb50c9ea118d195
Dumbez <kk1xyy@mail.ru>
2022-08-12 11:01:30 +0900

    update quali patch

--- src/pages/qualifications/edit.js
+++ src/pages/qualifications/edit.js
@@ -4,109 +4,113 @@ import { FileTwoTone } from "@ant-design/icons";
 import { useParams } from "react-router-dom";
 import { MyButton, Line } from "../../components";
 import {
-    useGetQualificationsIdQuery,
-    usePatchQualificationIdMutation,
+  useGetQualificationsIdQuery,
+  usePatchQualificationIdMutation,
 } from "../../services/QualificationsService";
 
 const { Text } = Typography;
 
 const QualificationEdit = () => {
-    const params = useParams();
+  const params = useParams();
 
-    const [patchQualificationId] = usePatchQualificationIdMutation();
-    const { data, isFetching, error } = useGetQualificationsIdQuery({
-        id: params.id,
-    });
-    if (isFetching) {
-        return (
-            <div
-                style={{
-                    height: 210,
-                    display: "flex",
-                    justifyContent: "center",
-                    paddingTop: 100,
-                }}
-            >
-                <Spin />
-                fsfds
-            </div>
-        );
-    }
-    console.log("detailq", data);
-
-    const inputs = [
-        {
-            title: "Номер документа:",
-            name: "id",
-        },
-        {
-            title: "Название квалификации:",
-            name: "name",
-        },
-        {
-            title: "Дата выдачи документа:",
-            name: "created",
-            type: "date",
-        },
-        {
-            title: "Начало срока:",
-            name: "date_start",
-        },
-        {
-            title: "Окончание срока:",
-            name: "date_finish",
-        },
-        // {
-        //     title: "Скан документа:",
-        //     name: "file",
-        // },
-    ];
-
-    const onSubmit = (data) => {
-        patchQualificationId({ id: params.id, body: data }).then((res) => {
-            if (res.data) {
-                message.success("Документ измсенен");
-            } else {
-                message.error(
-                    `${res.error.data.errors[0]} ${res.error.data.errors[1]}`
-                );
-            }
-        });
-    };
-
+  const [patchQualificationId] = usePatchQualificationIdMutation();
+  const { data, isFetching, error } = useGetQualificationsIdQuery({
+    id: params.id,
+  });
+  if (isFetching) {
     return (
-        <div>
-            <Form
-                initialValues={{
-                    // ["file"]: data.file,
-                    ["id"]: data.id,
-                    ["date_start"]: data.date_start,
-                    ["date_finish"]: data.date_finish,
-                    ["name"]: data.name,
-                    ["created"]: data.created,
-                }}
-                onFinish={onSubmit}
-            >
-                {inputs.map((item, index) => (
-                    <Form.Item
-                        key={index}
-                        label={
-                            <Text style={{ fontWeight: 600, fontSize: 16 }}>
-                                {item.title}
-                            </Text>
-                        }
-                        name={item.name}
-                        labelCol={{ span: 24 }}
-                        style={{ width: 350 }}
-                    >
-                        <Input placeholder={item.text} size="large" />
-                    </Form.Item>
-                ))}
-                <Line />
-                <MyButton htmlType="submit">Загрузить</MyButton>
-            </Form>
-        </div>
+      <div
+        style={{
+          height: 210,
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: 100,
+        }}
+      >
+        <Spin />
+        fsfds
+      </div>
     );
+  }
+  console.log("detailq", data);
+
+  const inputs = [
+    {
+      title: "Номер документа:",
+      name: "id",
+    },
+    {
+      title: "Название квалификации:",
+      name: "name",
+    },
+    {
+      title: "Дата выдачи документа:",
+      name: "created",
+      type: "date",
+    },
+    {
+      title: "Начало срока:",
+      name: "date_start",
+    },
+    {
+      title: "Окончание срока:",
+      name: "date_finish",
+    },
+    // {
+    //     title: "Скан документа:",
+    //     name: "file",
+    // },
+  ];
+
+  const onSubmit = (data) => {
+    let formData = new FormData();
+
+    formData.append("name", data.name);
+
+    patchQualificationId({ id: params.id, formData: formData }).then((res) => {
+      if (res) {
+        message.success("Документ изменен");
+      } else {
+        message.error(
+          `${res.error.data.errors[0]} ${res.error.data.errors[1]}`
+        );
+      }
+    });
+  };
+
+  return (
+    <div>
+      <Form
+        initialValues={{
+          //   ["file"]: data.file,
+          ["id"]: data.id,
+          //   ["date_start"]: data.date_start,
+          //   ["date_finish"]: data.date_finish,
+          ["name"]: data.name,
+          //   ["created"]: data.created.substring(0, 10),
+        }}
+        onFinish={onSubmit}
+      >
+        {inputs.map((item, index) => (
+          <Form.Item
+            key={index}
+            label={
+              <Text style={{ fontWeight: 600, fontSize: 16 }}>
+                {item.title}
+              </Text>
+            }
+            name={item.name}
+            labelCol={{ span: 24 }}
+            style={{ width: 350 }}
+          >
+            <Input placeholder={item.text} size="large" />
+          </Form.Item>
+        ))}
+        <Line />
+        <MyButton htmlType="submit">Загрузить</MyButton>
+      </Form>
+    </div>
+  );
 };
 
 export default QualificationEdit;
--- src/services/QualificationsService.js
+++ src/services/QualificationsService.js
@@ -1,29 +1,29 @@
 import { api } from "./api";
 
 export const qualifications = api.injectEndpoints({
-    endpoints: (build) => ({
-        getQualifications: build.query({
-            query: () => `users/qualification_improvement/`,
-            providesTags: ["Qualification"],
-        }),
-        getQualificationsId: build.query({
-            query: ({ id }) => `users/qualification_improvement/${id}`,
-        }),
-        patchQualificationId: build.mutation({
-            query({ id, body }) {
-                return {
-                    url: `users/qualification_improvement/${id}`,
-                    method: "PATCH",
-                    body,
-                };
-            },
-            invalidatesTags: [{ type: "Qualification" }],
-        }),
+  endpoints: (build) => ({
+    getQualifications: build.query({
+      query: () => `users/qualification_improvement/`,
+      providesTags: ["Qualification"],
     }),
+    getQualificationsId: build.query({
+      query: ({ id }) => `users/qualification_improvement/${id}`,
+    }),
+    patchQualificationId: build.mutation({
+      query({ id, formData }) {
+        return {
+          url: `users/qualification_improvement/${id}`,
+          method: "PATCH",
+          formData,
+        };
+      },
+      invalidatesTags: [{ type: "Qualification" }],
+    }),
+  }),
 });
 
 export const {
-    useGetQualificationsQuery,
-    useGetQualificationsIdQuery,
-    usePatchQualificationIdMutation,
+  useGetQualificationsQuery,
+  useGetQualificationsIdQuery,
+  usePatchQualificationIdMutation,
 } = qualifications;
