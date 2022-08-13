3a61c685cf70533fcdca470a22ca527d889afcf9
mrmainly <ernest_everstov@mail.ru>
2022-08-14 06:02:00 +0900

    adde\

--- src/layout/index.js
+++ src/layout/index.js
@@ -14,112 +14,92 @@ const { Content, Sider } = Layout;
 const { Title, Text } = Typography;
 
 const MyLayout = () => {
-    const navigate = useNavigate();
-    const params = useLocation();
+  const navigate = useNavigate();
+  const params = useLocation();
 
-    const items = [
+  const items = [
+    {
+      label: "Тестирование",
+      key: "submenu-2",
+      icon: <BsCardChecklist />,
+      children: [
         {
-            label: "Иванов Иван",
-            key: "submenu-1",
-            icon: <BsPersonFill />,
-            children: [
-                {
-                    label: "Настройка профиля",
-                    key: "submenu-item-1",
-                    onClick: () => navigate(ROUTES.PROFILE),
-                },
-                {
-                    label: "Выйти из системы",
-                    key: "submenu-item-2",
-                    onClick: () => {
-                        localStorage.removeItem("jwttoken");
-                        navigate(ROUTES.LOGIN);
-                    },
-                },
-            ],
+          label: "Подача заявления",
+          key: "submenu-item-4",
+          onClick: () => navigate(ROUTES.APPILYNG),
         },
         {
-            label: "Тестирование",
-            key: "submenu-2",
-            icon: <BsCardChecklist />,
-            children: [
-                {
-                    label: "Подача заявления",
-                    key: "submenu-item-4",
-                    onClick: () => navigate(ROUTES.APPILYNG),
-                },
-                {
-                    label: "Доступные тесты",
-                    key: "submenu-item-3",
-                    onClick: () => navigate(ROUTES.AVAILABLE_TESTS),
-                },
-                {
-                    label: "Итоги аттестации",
-                    key: "submenu-item-7",
-                    onClick: () => navigate(ROUTES.CERTIFICATION_RESULTS),
-                },
-            ],
+          label: "Доступные тесты",
+          key: "submenu-item-3",
+          onClick: () => navigate(ROUTES.AVAILABLE_TESTS),
         },
         {
-            label: "Документы",
-            key: "submenu-3",
-            icon: <HiOutlineDocumentText />,
-            children: [
-                {
-                    label: "Загрузить документы",
-                    key: "submenu-item-5",
-                    onClick: () => navigate(ROUTES.DOCUMENTS),
-                },
-                {
-                    label: "Мои квалификации",
-                    key: "submenu-item-6",
-                    onClick: () => navigate(ROUTES.MY_QUALIFICATIONS),
-                },
-            ],
+          label: "Итоги аттестации",
+          key: "submenu-item-7",
+          onClick: () => navigate(ROUTES.CERTIFICATION_RESULTS),
         },
-    ];
+      ],
+    },
+    {
+      label: "Документы",
+      key: "submenu-3",
+      icon: <HiOutlineDocumentText />,
+      children: [
+        {
+          label: "Загрузить документы",
+          key: "submenu-item-5",
+          onClick: () => navigate(ROUTES.DOCUMENTS),
+        },
+        {
+          label: "Мои квалификации",
+          key: "submenu-item-6",
+          onClick: () => navigate(ROUTES.MY_QUALIFICATIONS),
+        },
+      ],
+    },
+  ];
 
-    return (
-        <>
-            {params.pathname == "/" ||
-            params.pathname == "/registration" ||
-            params.pathname == "/forgot-password" ? (
-                <div>
-                    <Outlet />
-                </div>
-            ) : (
-                <Layout style={{ minHeight: 950 }}>
-                    <Sider
-                        width={250}
-                        breakpoint="lg"
-                        collapsedWidth="0"
-                        className="site-layout-background"
-                        style={{ background: "#09304A" }}
-                    >
-                        <Menu
-                            mode="inline"
-                            style={{ background: "#09304A", color: "white" }}
-                            items={items}
-                            theme="dark"
-                        />
-                        <Divider style={{ background: "white" }} />
-                    </Sider>
+  return (
+    <>
+      {params.pathname == "/" ||
+      params.pathname == "/registration" ||
+      params.pathname == "/forgot-password" ? (
+        <div>
+          <Outlet />
+        </div>
+      ) : (
+        <Layout style={{ minHeight: 950 }}>
+          <Sider
+            width={250}
+            breakpoint="lg"
+            collapsedWidth="0"
+            className="site-layout-background"
+            style={{ background: "#09304A" }}
+          >
+            <Menu
+              mode="inline"
+              style={{ background: "#09304A", color: "white", marginTop: 20 }}
+              items={items}
+              theme="dark"
+            />
+            <Divider style={{ background: "white" }} />
+          </Sider>
 
-                    {params.pathname === ROUTES.SURVEYS ? (
-                        <SurveyLayout />
-                    ) : (
-                        <MainLayout params={params} />
-                    )}
-                </Layout>
-            )}
-        </>
-    );
+          {params.pathname === ROUTES.SURVEYS ? (
+            <SurveyLayout />
+          ) : (
+            <MainLayout params={params} />
+          )}
+        </Layout>
+      )}
+    </>
+  );
 };
 
 export default MyLayout;
 
 {
-    /* {items.map((item, index) => (
+  /* {items.map((item, index) => (
                                 <Menu.Item
                                     key={index}
                                     onClick={() => navigate(item.navigate)}
--- src/pages/documents/detail.js
+++ src/pages/documents/detail.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Typography, Form, Input, Upload, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+
+import { MyButton, Line } from "../../components";
+
+const { Text, Title } = Typography;
 
 const DocumentDetail = () => {
-    return <div></div>;
+  return (
+    <div>
+      <Text style={{ fontSize: 18 }}>Паспорт</Text>
+      <Form>
+        <Form.Item
+          label={
+            <Text style={{ fontWeight: 600, fontSize: 16 }}>
+              Загрузить документ
+            </Text>
+          }
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your login!",
+            },
+          ]}
+          labelCol={{ span: 24 }}
+          style={{ width: 300 }}
+        >
+          <Upload>
+            {" "}
+            <Button icon={<UploadOutlined />}>Upload</Button>
+          </Upload>
+        </Form.Item>
+      </Form>
+      <Line />
+      <div style={{ display: "flex" }}>
+        <MyButton>Сохранить</MyButton>
+        <MyButton style={{ marginLeft: 20, background: "#6C757D" }}>
+          Отмена
+        </MyButton>
+      </div>
+    </div>
+  );
 };
 
 export default DocumentDetail;
