e5107b7cdad6400060de76dbcb1587cc8d48fe7c
ernest <ernest_everstov@mail.ru>
2022-08-16 13:42:57 +0900

    added editor diploma

--- src/pages/documents/detail/components/Diploma.js
+++ src/pages/documents/detail/components/Diploma.js
@@ -1,25 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { UploadOutlined, DeleteOutlined } from "@ant-design/icons";
 
-import { Form, Typography, Input, Upload } from "antd";
+import { Form, Typography, Input, Upload, Button } from "antd";
 
 const { Text, Title } = Typography;
 const { TextArea } = Input;
 
-const Diploma = () => {
-  const inputs = [
-    {
-      name: "diploma",
-    },
-  ];
-  return (
-    <div style={{ width: 350 }}>
-      {inputs.map((item, index) => (
-        <Form.Item key={index} name={item.name} label={<Text>Описание</Text>}>
-          {/* <Input type="file" /> */}
-        </Form.Item>
-      ))}
-    </div>
-  );
+const Diploma = ({ data }) => {
+    const [localData, setLocalData] = useState(
+        JSON.parse(JSON.stringify(data))
+    );
+    const [photo, setPhoto] = useState();
+    let [idCounter, setIdCounter] = useState(data.length);
+
+    const addInput = async (e) => {
+        setIdCounter((idCounter += 1));
+        let newInput = localData.concat({
+            created: "2022-08-15T18:56:39.473183",
+            document: "",
+            file: "https://notalone.medic.fun/media/diploma/%D0%B4%D0%B8%D0%BF%D0%BB%D0%BE%D0%BC_1.pdf",
+            id: idCounter,
+            is_verify: false,
+            name: "",
+            updated: "2022-08-15T18:58:26.556764",
+        });
+        setLocalData(newInput);
+    };
+
+    const handleText = (id) => (e) => {
+        let newLocalData = [...localData];
+        newLocalData[id - 1].name = e.target.value;
+        // console.log((newLocalData[0].document = 123));
+        setLocalData(newLocalData);
+    };
+
+    const handleFile = (id) => (e) => {
+        let newLocalData = [...localData];
+        newLocalData[id - 1].file = e.file.originFileObj;
+        setLocalData(newLocalData);
+    };
+
+    const handleDelete = (i) => {
+        setIdCounter(idCounter - 1);
+        setLocalData(localData.filter((tag) => tag.id !== i));
+    };
+
+    return (
+        <div style={{ width: 350 }}>
+            <Text style={{ fontSize: 18 }}>Диплом</Text>
+            {localData.map((item, index) => (
+                <div
+                    key={index}
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginTop: 20,
+                    }}
+                >
+                    <input
+                        style={{ marginRight: 20 }}
+                        placeholder="Описание"
+                        value={item.name}
+                        onChange={handleText(item.id)}
+                    />
+                    <Upload
+                        listType=""
+                        multiple={false}
+                        maxCount={1}
+                        onChange={handleFile(item.id)}
+                    >
+                        <Button icon={<UploadOutlined />}>Upload</Button>
+                    </Upload>
+                    <a href={item.file} style={{ marginLeft: 20 }}>
+                        document.pdf
+                    </a>
+                    <DeleteOutlined
+                        style={{ marginLeft: 20, color: "red" }}
+                        onClick={() => handleDelete(item.id)}
+                    />
+                </div>
+            ))}
+            <Button style={{ marginTop: 20 }} onClick={() => addInput()}>
+                + Добавить документ
+            </Button>
+            <Button onClick={() => console.log(localData)}>asd</Button>
+        </div>
+    );
 };
 
 export default Diploma;
--- src/pages/documents/detail/index.js
+++ src/pages/documents/detail/index.js
@@ -1,77 +1,72 @@
 import React from "react";
 import {
-  Typography,
-  Form,
-  message,
-  Input,
-  Upload,
-  Button,
-  Space,
-  Spin,
+    Typography,
+    Form,
+    message,
+    Input,
+    Upload,
+    Button,
+    Space,
+    Spin,
 } from "antd";
 import {
-  UploadOutlined,
-  FileTwoTone,
-  MinusCircleOutlined,
-  PlusOutlined,
+    UploadOutlined,
+    FileTwoTone,
+    MinusCircleOutlined,
+    PlusOutlined,
 } from "@ant-design/icons";
 
 import Diploma from "./components/Diploma";
 import { Line, MyButton } from "../../../components";
 import {
-  useGetDocumentsQuery,
-  usePostDocumentsMutation,
-  usePostDocumentsDiplomaMutation,
-  usePatchDocumentsDiplomaMutation,
+    useGetDocumentsQuery,
+    usePostDocumentsMutation,
+    usePostDocumentsDiplomaMutation,
+    usePatchDocumentsDiplomaMutation,
 } from "../../../services/DocumentsService";
 
 const DocumentDetail = () => {
-  const { data, isFetching, error } = useGetDocumentsQuery("");
-  const [patchDocumentsDiploma] = usePatchDocumentsDiplomaMutation();
+    const { data, isFetching, error } = useGetDocumentsQuery("");
+    const [patchDocumentsDiploma] = usePatchDocumentsDiplomaMutation();
 
-  const onSubmit = (data) => {
-    patchDocumentsDiploma(data).then((res) => {
-      if (res.data) {
-        message.success("Профиль изменен");
-      } else {
-        message.error(
-          `${res.error.data.errors[0]} ${res.error.data.errors[1]}`
-        );
-      }
-      console.log(res);
-    });
-    console.log(data);
-  };
+    const onSubmit = (data) => {
+        patchDocumentsDiploma(data).then((res) => {
+            if (res.data) {
+                message.success("Профиль изменен");
+            } else {
+                message.error(
+                    `${res.error.data.errors[0]} ${res.error.data.errors[1]}`
+                );
+            }
+            console.log(res);
+        });
+        console.log(data);
+    };
 
-  console.log("file", data?.diploma[0].file);
+    console.log("file", data?.diploma);
 
-  return (
-    <div>
-      {isFetching ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            height: "100vh",
-            alignItems: "center",
-          }}
-        >
-          <Spin />
+    return (
+        <div>
+            {isFetching ? (
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        height: "100vh",
+                        alignItems: "center",
+                    }}
+                >
+                    <Spin />
+                </div>
+            ) : (
+                <div>
+                    <Diploma data={data.diploma} />
+                    <Line />
+                    <MyButton htmlType="submit">Сохранить</MyButton>
+                </div>
+            )}
         </div>
-      ) : (
-        <Form
-          onFinish={onSubmit}
-          initialValues={{
-            ["passport"]: data.passport,
-            ["diploma"]: data.diploma[0].file,
-          }}
-        >
-          <Diploma />
-          <MyButton htmlType="submit">Сохранить</MyButton>
-        </Form>
-      )}
-    </div>
-  );
+    );
 };
 
 export default DocumentDetail;
