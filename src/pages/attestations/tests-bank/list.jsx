aa996829a405960595c06ce08a56d601506c3c33
Dumbez <kk1xyy@mail.ru>
2022-09-07 11:51:32 +0900

    added filters quali attest

--- src/pages/attestations/tests-bank/list.jsx
+++ src/pages/attestations/tests-bank/list.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
-import { Pagination } from 'antd'
+import { Pagination, Input, Select } from 'antd'
 
 import TBAddModal from '../components/modals/tbaddmodal'
-import { useGetAttestationsTestsBankQuery } from '../../../services/AttestationService'
+import {
+    useGetAttestationsTestsBankQuery,
+    useGetAttestationsQualificationQuery,
+} from '../../../services/AttestationService'
 
 import TestsBankTable from '../components/tables/TestsBankTable'
 import { MyButton } from '../../../components'
@@ -10,7 +13,21 @@ import { MyButton } from '../../../components'
 const TestsBank = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [totalPage, setTotalPage] = useState(30)
-    const { data, isLoading } = useGetAttestationsTestsBankQuery({ currentPage: currentPage })
+    const [name, setName] = useState('')
+    const [status, setStatus] = useState('')
+    const [directionName, setDirectionName] = useState('')
+    const { data, isLoading } = useGetAttestationsTestsBankQuery({
+        currentPage: currentPage,
+        name: name,
+        is_active: status,
+        direction_name: directionName,
+    })
+    const { data: directionSelect } = useGetAttestationsQualificationQuery({
+        is_active: '',
+        name: '',
+        tag: '',
+        id: '',
+    })
     const [modalNewTest, setModalNewTest] = useState(false)
     const onChange = (page) => {
         setCurrentPage(page)
@@ -18,11 +35,62 @@ const TestsBank = () => {
     useEffect(() => {
         setTotalPage(data?.count)
     }, [data])
+    console.log(directionSelect)
     return (
         <div>
             <MyButton onClick={() => setModalNewTest(true)} style={{ marginBottom: 20 }}>
                 Создать новый тест
             </MyButton>
+            <div
+                style={{
+                    display: 'flex',
+                    marginBottom: 16,
+                    justifyContent: 'start',
+                    gap: 16,
+                }}
+            >
+                <Input.Search
+                    placeholder="Название квалификации"
+                    enterButton
+                    onSearch={(value) => {
+                        const currValue = value
+                        setName(currValue)
+                    }}
+                    style={{
+                        width: '33%',
+                    }}
+                ></Input.Search>
+                <Select
+                    showSearch
+                    style={{
+                        width: 200,
+                    }}
+                    placeholder="Search to Select"
+                    optionFilterProp="children"
+                    filterOption={(input, option) => option.children.includes(input)}
+                    filterSort={(optionA, optionB) =>
+                        optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())
+                    }
+                    onChange={(value) => setDirectionName(value)}
+                >
+                    {directionSelect?.results?.map((item, index) => (
+                        <Select.Option value={item.name} key={index}>
+                            {item.name}
+                        </Select.Option>
+                    ))}
+                </Select>
+                <Select
+                    placeholder="Статус"
+                    style={{
+                        width: '33%',
+                    }}
+                    onChange={(value) => setStatus(value)}
+                >
+                    <Select.Option value="">Все статусы</Select.Option>
+                    <Select.Option value="true">Активна</Select.Option>
+                    <Select.Option value="false">Не активна</Select.Option>
+                </Select>
+            </div>
             <TBAddModal open={modalNewTest} setOpen={setModalNewTest} />
             <TestsBankTable data={data?.results} loading={isLoading} />
             <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
