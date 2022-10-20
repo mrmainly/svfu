cd06a13bbc531071b6adae51acf1473ff440e3cd
mrmainly <ernest_everstov@mail.ru>
2022-10-20 22:44:34 +0900

    fixed view expert

--- src/constructor/parts/soft_test_expert_moderator.jsx
+++ src/constructor/parts/soft_test_expert_moderator.jsx
@@ -67,46 +67,6 @@ const SoftTestExMo = ({ surveyquest }) => {
 
                             <Table columns={columns} dataSource={item.question.variant} />
 
-                            <Text style={{ marginTop: 20, fontWeight: 'bold' }}>
-                                Варианты ответа:
-                            </Text>
-                            <div style={{ marginTop: 10 }}>
-                                {item.question.technique === 'ONE_CHOICE' ? (
-                                    <Space direction="vertical">
-                                        {item.question.variant.map((item, index) => (
-                                            <Text key={index}>
-                                                {item.name}
-                                                <span style={{ marginLeft: 3, fontWeight: 'bold' }}>
-                                                    {item.is_true === true && '(правильный)'}
-                                                </span>
-                                            </Text>
-                                        ))}
-                                    </Space>
-                                ) : (
-                                    <div
-                                        style={{
-                                            display: 'flex',
-                                            flexDirection: 'column',
-                                            marginTop: '-10px',
-                                        }}
-                                    >
-                                        {item.question.variant.map((item, index) => (
-                                            <Text
-                                                style={{
-                                                    marginTop: 10,
-                                                    marginLeft: 1,
-                                                }}
-                                                key={index}
-                                            >
-                                                {item.name}
-                                                <span style={{ marginLeft: 3, fontWeight: 'bold' }}>
-                                                    {item.is_true === true && '(правильный)'}
-                                                </span>
-                                            </Text>
-                                        ))}
-                                    </div>
-                                )}
-                            </div>
                             <div
                                 style={{
                                     height: 1,
@@ -122,27 +82,6 @@ const SoftTestExMo = ({ surveyquest }) => {
                                     flexDirection: 'column',
                                 }}
                             >
-                                <div
-                                    style={{
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                    }}
-                                >
-                                    <Text style={{ fontWeight: 'bold' }}>Ответ аттестуемого:</Text>
-                                    {surveyquest?.answers_first_part?.map((itemAnswer, index) => {
-                                        if (itemAnswer.question_id === item.question.id)
-                                            return <Text key={index}>{itemAnswer.answer}</Text>
-                                    })}
-                                </div>
-                                <div
-                                    style={{
-                                        height: 1,
-                                        background: '#E6E6E6',
-                                        width: '100%',
-                                        marginTop: 20,
-                                        marginBottom: 20,
-                                    }}
-                                />
                                 <Space
                                     style={{
                                         display: 'flex',
