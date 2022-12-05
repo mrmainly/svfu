c390bc8191f8898d115cd9a786816e05e11feb2b
DUMBEZ <kk1xyy@mail.ru>
2022-12-05 23:20:16 +0900

    fixed bag

--- src/components/sidebars/surveySideBarTester/index.jsx
+++ src/components/sidebars/surveySideBarTester/index.jsx
@@ -5,7 +5,6 @@ import '../surveySideBar.css'
 import { useGetSurveyIdQuery } from '../../../services/tester/Surveys'
 import HardBodyTester from './components/hard_body'
 import SoftBodyTester from './components/soft_body'
-import { useGetSurveyIdQuery } from '../../../services/tester/Surveys'
 
 const SurveySideBarTester = () => {
     const { data, isFetching } = useGetSurveyIdQuery({
